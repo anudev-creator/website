ffbf3c56abe7a54373d2dabcf25d819997f18441
anudev-creator <159486828+anudev-creator@users.noreply.github.com>
2024-06-16 13:39:25 +0530

    started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React,{ Component } from 'react';
+import Landing from './Components/Landing/Landing';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <Landing/>,
+  },
+]);
+
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+      <RouterProvider router={router} />
+
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
